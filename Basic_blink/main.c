21869e8fb591c1435725bc5e808f755d8efc3929
Almighty42 <xenoman4202@gmail.com>
2026-01-06 23:03:29 +0100

    Set up the initial code, as well as C code in main that may or may not work, have to test later.

---
+++ Basic_blink/main.c
@@ -0,0 +1,32 @@
+#include <stdint.h>
+
+#define RCC_BASE 0x40023800
+#define RCC_AHB1ENR RCC_BASE + 0x30000000
+
+#define GPIOA_BASE 0x40020000
+#define GPIOA_MODER GPIOA_BASE
+#define GPIOA_ODR GPIOA_BASE + 0x14000000
+
+int main(void)
+{
+	// Enabling GPIOA Clock ( RCC )
+	uint32_t* gpioa_clock = (uint32_t*)(RCC_AHB1ENR);
+	*gpioa_clock |= (1u << 0);
+
+	// Configuring PA5 as OUTPUT
+	uint32_t* gpioa_moder = (uint32_t*)(GPIOA_MODER);
+	*gpioa_moder |= (1u << 10);
+
+	// Flipping PA5 value
+	uint32_t* gpioa_odr = (uint32_t*)(GPIOA_ODR);
+	while (1) {
+		// Sets PA5 to ON
+		*gpioa_odr |= (1u << 5);
+		for (uint32_t i = 0; i < 1000; i++) {
+		}
+		// Sets PA5 to OFF
+		*gpioa_odr &= ~(1u << 5);
+		for (uint32_t i = 0; i < 1000; i++) {
+		}
+	}
+}
