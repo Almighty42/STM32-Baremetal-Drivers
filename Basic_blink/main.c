48346ef814136e5e3e2c23076d1ffb21cff299c1
Almighty42 <xenoman4202@gmail.com>
2026-01-09 23:22:38 +0100

    Basic blink works, added volatile to hardware register pointers in main.c, also finished Makefile for Basic Blink

--- Basic_blink/main.c
+++ Basic_blink/main.c
@@ -10,24 +10,24 @@
 int main(void)
 {
 	// Enabling GPIOA Clock ( RCC )
-	uint32_t* gpioa_clock = (uint32_t*)(RCC_AHB1ENR);
+	uint32_t* volatile gpioa_clock = (uint32_t*)(RCC_AHB1ENR);
 	*gpioa_clock |= (1u << 0);
 
 	// Configuring PA5 as OUTPUT
-	uint32_t* gpioa_moder = (uint32_t*)(GPIOA_MODER);
+	uint32_t* volatile gpioa_moder = (uint32_t*)(GPIOA_MODER);
 	*gpioa_moder &= ~(3u << 10);
 	*gpioa_moder |= (1u << 10);
 
 	// Flipping PA5 value
-	uint32_t* gpioa_odr = (uint32_t*)(GPIOA_ODR);
+	uint32_t* volatile gpioa_odr = (uint32_t*)(GPIOA_ODR);
 	while (1) {
 		// Sets PA5 to ON
 		*gpioa_odr |= (1u << 5);
-		for (volatile uint32_t i = 0; i < 10000; i++) {
+		for (volatile uint32_t i = 0; i < 1000000; i++) {
 		}
 		// Sets PA5 to OFF
 		*gpioa_odr &= ~(1u << 5);
-		for (volatile uint32_t i = 0; i < 10000; i++) {
+		for (volatile uint32_t i = 0; i < 1000000; i++) {
 		}
 	}
 }
