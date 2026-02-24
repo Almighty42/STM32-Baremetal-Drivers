aca1e5d38f1aa20dd1c5ab6fbc27b43544077c01
Almighty42 <xenoman4202@gmail.com>
2026-02-24 10:35:29 +0100

    Started working on SPI and I2C drivers, still very incomplete

---
+++ Generic/Drivers/SPI_driver/stm32f401xx_SPI_driver.c
@@ -0,0 +1,339 @@
+#include "stm32f401xx_SPI_driver.h"
+#include "stm32f401xx.h"
+
+/********************************************************************************
+ *
+ * TODO: Future plans for this driver:
+ * 1. ...
+ *
+ *******************************************************************************/
+
+/********************************************************************************
+ *
+ * INFO: Driver map
+ * @PERIPHERAL_CLOCK_SETUP
+ * @INIT_DE-INIT
+ * @DATA_SEND_RECEIVE_POLLING_INTERRUPT
+ * @PERIPHERAL_CONTROL_API
+ * @IRQ_CONFIGURATION_AND_ISR_HANDLING
+ *
+ *******************************************************************************/
+
+// NOTE: @PERIPHERAL_CLOCK_SETUP
+
+/********************************************************************************
+ * @fn				- SPI_peri_clk_control
+ *
+ * @brief			- Enables or disables peripheral clock for SPI
+ *
+ * @param[*p_USART_x]		- Base address of the SPI peripheral
+ * @param[EN_or_DI]		- ENABLE or DISABLE macros
+ *
+ * @return			- Success / Failure status of the function
+ *
+ * @Note			- None
+ *******************************************************************************/
+
+SPI_status_t SPI_peri_clk_control(SPI_TypeDef* p_SPI_x, uint8_t EN_or_DI)
+{
+	VALIDATE_EN_DI(EN_or_DI, SPI_ERROR_INVALID_STATE);
+
+	SPI_status_t status = SPI_ERROR_INVALID_PORT;
+
+	if (p_SPI_x == SPI1) {
+		if (EN_or_DI == ENABLE) {
+			SPI1_PCLK_EN();
+		}
+		else
+			SPI1_PCLK_DI();
+		status = SPI_OK;
+	}
+	else if (p_SPI_x == SPI2) {
+		if (EN_or_DI == ENABLE) {
+			SPI2_PCLK_EN();
+		}
+		else
+			SPI2_PCLK_DI();
+		status = SPI_OK;
+	}
+	else if (p_SPI_x == SPI3) {
+		if (EN_or_DI == ENABLE) {
+			SPI3_PCLK_EN();
+		}
+		else
+			SPI3_PCLK_DI();
+		status = SPI_OK;
+	}
+	else if (p_SPI_x == SPI4) {
+		if (EN_or_DI == ENABLE) {
+			SPI4_PCLK_EN();
+		}
+		else
+			SPI4_PCLK_DI();
+		status = SPI_OK;
+	}
+
+	return status;
+}
+
+// NOTE: @INIT_DE-INIT
+
+/********************************************************************************
+ * @fn				- SPI_init
+ *
+ * @brief			- Initializes a SPI peripheral
+ *
+ * @param[*p_USART_handle]	- Handle structure of a SPI peripheral
+ *
+ * @return			- Success / Failure status of the function
+ *
+ * @Note			- None
+ *******************************************************************************/
+
+SPI_status_t SPI_init(SPI_Handle_t* p_SPI_handle)
+{
+	VALIDATE_PTR(p_SPI_handle, SPI_ERROR_NULL_PTR);
+
+	SPI_TypeDef* port = p_SPI_handle->p_SPIx;
+	VALIDATE_SPI_PORT(port);
+
+	volatile uint32_t* cr1 = &p_SPI_handle->p_SPIx->CR1;
+	volatile uint32_t* cr2 = &p_SPI_handle->p_SPIx->CR2;
+
+	// Clock
+	SPI_peri_clk_control(port, ENABLE);
+
+	// Mode
+	uint8_t mode = p_SPI_handle->SPI_Config.SPI_Device_Mode;
+
+	VALIDATE_SPI_DEVICE_MODE(mode);
+
+	CLEAR_BIT(*cr1, SPI_CR1_MSTR);
+	if (mode == SPI_DEVICE_MODE_MASTER)
+		SET_BIT(*cr1, SPI_CR1_MSTR);
+	else if (mode == SPI_DEVICE_MODE_SLAVE)
+		CLEAR_BIT(*cr1, SPI_CR1_MSTR);
+
+	// WARNING: Have to be tested might be wrong
+	// Bus config
+	uint8_t bus_config = p_SPI_handle->SPI_Config.SPI_Bus_Config;
+
+	VALIDATE_SPI_BUS_CONFIG(bus_config);
+
+	if (bus_config == SPI_BUS_CONFIG_FD)
+		CLEAR_BIT(*cr1, SPI_CR1_BIDIMODE);
+	else if (bus_config == SPI_BUS_CONFIG_HD) {
+		SET_BIT(*cr1, SPI_CR1_BIDIMODE);
+	}
+	else if (bus_config == SPI_BUS_CONFIG_SIMPLEX_RX_ONLY) {
+		CLEAR_BIT(*cr1, SPI_CR1_BIDIMODE);
+		SET_BIT(*cr1, SPI_CR1_RXONLY);
+	}
+	else if (bus_config == SPI_BUS_CONFIG_SIMPLEX_TX_ONLY) {
+		CLEAR_BIT(*cr1, SPI_CR1_BIDIMODE);
+		CLEAR_BIT(*cr1, SPI_CR1_RXONLY);
+	}
+
+	// TODO: Speed
+	uint8_t speed = p_SPI_handle->SPI_Config.SPI_SCLK_Speed;
+
+	VALIDATE_SPI_SCLK_SPEED(speed);
+
+	// DFF
+	uint8_t dff = p_SPI_handle->SPI_Config.SPI_DFF;
+
+	VALIDATE_SPI_DFF(dff);
+
+	CLEAR_BIT(*cr1, SPI_CR1_DFF);
+	if (dff == SPI_DFF_16BIT)
+		SET_BIT(*cr1, SPI_CR1_DFF);
+	else if (dff == SPI_DFF_8BIT)
+		CLEAR_BIT(*cr1, SPI_CR1_DFF);
+
+	// CPOL
+	uint8_t cpol = p_SPI_handle->SPI_Config.SPI_CPOL;
+
+	VALIDATE_SPI_CPOL(cpol);
+
+	CLEAR_BIT(*cr1, SPI_CR1_CPOL);
+	if (cpol == SPI_CPOL_HIGH)
+		SET_BIT(*cr1, SPI_CR1_CPOL);
+	else if (cpol == SPI_CPOL_LOW)
+		CLEAR_BIT(*cr1, SPI_CR1_CPOL);
+
+	// CPHA
+	uint8_t cpha = p_SPI_handle->SPI_Config.SPI_CPHA;
+
+	VALIDATE_SPI_CPHA(cpha);
+
+	CLEAR_BIT(*cr1, SPI_CR1_CPHA);
+	if (cpol == SPI_CPHA_HIGH)
+		SET_BIT(*cr1, SPI_CR1_CPHA);
+	else if (cpol == SPI_CPHA_LOW)
+		CLEAR_BIT(*cr1, SPI_CR1_CPHA);
+
+	// SSM
+	uint8_t ssm = p_SPI_handle->SPI_Config.SPI_SSM;
+
+	VALIDATE_SPI_SSM(ssm);
+
+	CLEAR_BIT(*cr1, SPI_CR1_SSM);
+	if (cpol == SPI_SSM_SW)
+		SET_BIT(*cr1, SPI_CR1_SSM);
+	else if (cpol == SPI_SSM_HW)
+		CLEAR_BIT(*cr1, SPI_CR1_SSM);
+
+	return SPI_OK;
+}
+
+/********************************************************************************
+ * @fn				- SPI_de_init
+ *
+ * @brief			- Resets a SPI peripheral
+ *
+ * @param[*p_USART_x]		- Base address of the SPI peripheral
+ *
+ * @return			- Success / Failure status of the function
+ *
+ * @Note			- None
+ *******************************************************************************/
+
+SPI_status_t SPI_de_init(SPI_TypeDef* p_SPI_x);
+
+// NOTE: @DATA_SEND_RECEIVE_POLLING_INTERRUPT
+
+/********************************************************************************
+ * @fn				- SPI_write_data_pl
+ *
+ * @brief			- Write data over USART using a polling method
+ *
+ * @param[*p_SPI_x]		- Base address of the SPI peripheral
+ * @param[*p_tx_buffer]		- Transmission buffer ( pointer )
+ * @param[len]			- Number of bytes to send
+ *
+ * @return			- Success / Failure status of the function
+ *
+ * @Note			- None
+ *******************************************************************************/
+
+SPI_status_t SPI_write_data_pl(SPI_Handle_t* p_SPI_handle,
+                               const uint8_t* p_tx_buffer, uint32_t len)
+{
+	while (len != 0) {
+		while (!IS_BIT_SET(p_SPI_handle->p_SPIx->SR, SPI_SR_TXE)) {
+			if (IS_BIT_SET(p_SPI_handle->p_SPIx->CR1,
+			               SPI_CR1_DFF)) {
+				p_SPI_handle->p_SPIx->DR =
+				    *((uint16_t*)p_tx_buffer);
+				len -= 2;
+				p_tx_buffer += 2;
+			}
+			else {
+				p_SPI_handle->p_SPIx->DR = *(p_tx_buffer);
+				len--;
+				p_tx_buffer++;
+			}
+		}
+	}
+
+	return SPI_OK;
+}
+
+/********************************************************************************
+ * @fn				- SPI_read_data_pl
+ *
+ * @brief			- Reads data over SPI using a polling method
+ *
+ * @param[*p_SPI_x]		- Base address of the SPI peripheral
+ * @param[*p_rx_buffer]		- Receive buffer ( pointer )
+ * @param[len]			- Number of bytes to receive
+ *
+ * @return			- Success / Failure status of the function
+ *
+ * @Note			- None
+ *******************************************************************************/
+
+SPI_status_t SPI_read_data_pl(SPI_Handle_t* p_SPI_handle, uint8_t* p_rx_buffer,
+                              uint32_t len);
+
+/********************************************************************************
+ * @fn				- SPI_write_data_it
+ *
+ * @brief			- Enables or disables peripheral clock for USART
+ *
+ * @param[*p_SPI_handle]	- Handle structure of a SPI peripheral
+ * @param[*p_tx_buffer]		- Transmission buffer ( pointer )
+ * @param[len]			- Number of bytes to send
+ *
+ * @return			- Success / Failure status of the function
+ *
+ * @Note			- None
+ *******************************************************************************/
+
+SPI_status_t SPI_write_data_it(SPI_Handle_t* p_SPI_handle, uint8_t* p_tx_buffer,
+                               uint32_t len);
+
+/********************************************************************************
+ * @fn				- SPI_read_data_it
+ *
+ * @brief			- Reads data over SPI via interrupts
+ *
+ * @param[*p_SPI_handle]	- Handle structure of a SPI peripheral
+ * @param[*p_rx_buffer]		- Receive buffer ( pointer )
+ * @param[len]			- Number of bytes to receive
+ *
+ * @return			- Success / Failure status of the function
+ *
+ * @Note			- None
+ *******************************************************************************/
+
+SPI_status_t SPI_read_data_it(SPI_Handle_t* p_SPI_handle, uint8_t* p_rx_buffer,
+                              uint32_t len);
+
+// NOTE: @PERIPHERAL_CONTROL_API
+
+// NOTE: IRQ_CONFIGURATION_AND_ISR_HANDLING
+
+/********************************************************************************
+ * @fn				- SPI_irq_interrupt_config
+ *
+ * @brief			- Configures SPI IRQ interrupt
+ *
+ * @param[irq_n]		- IRQ number
+ * @param[EN_or_DI]		- ENABLE or DISABLE macros
+ *
+ * @return			- Success / Failure status of the function
+ *
+ * @Note			- None
+ *******************************************************************************/
+
+SPI_status_t SPI_irq_interrupt_config(uint8_t irq_n, uint8_t EN_or_DI);
+
+/********************************************************************************
+ * @fn				- SPI_irq_priority_config
+ *
+ * @brief			- Configures SPI IRQ priority
+ *
+ * @param[irq_n]		- IRQ number
+ * @param[irq_prio]		- IRQ priority
+ *
+ * @return			- Success / Failure status of the function
+ *
+ * @Note			- None
+ *******************************************************************************/
+
+SPI_status_t SPI_irq_priority_config(uint8_t irq_n, uint32_t irq_prio);
+
+/********************************************************************************
+ * @fn				- SPI_irq_handling
+ *
+ * @brief			- Handles SPI interrupts
+ *
+ * @param[*p_SPI_handle]	- Handle structure of a SPI peripheral
+ *
+ * @return			- None
+ *
+ * @Note			- None
+ *******************************************************************************/
+
+void SPI_irq_handling(SPI_Handle_t* p_SPI_handle);
---
+++ Generic/Drivers/SPI_driver/stm32f401xx_SPI_driver.h
@@ -0,0 +1,167 @@
+#ifndef STM32F401RE_SPI_DRIVER_H
+#define STM32F401RE_SPI_DRIVER_H
+
+#include "stm32f401xx.h"
+
+// NOTE: --- Structures for SPI ---
+
+// Configuration structure for a SPI
+
+typedef struct
+{
+	uint8_t SPI_Device_Mode;					// Possible values from @SPI_DEVICE_MODE
+	uint8_t SPI_Bus_Config;						// Possible values from @SPI_BUS_CONFIG
+	uint8_t SPI_SCLK_Speed;						// Possible values from @SPI_SCLK_SPEED
+	uint8_t SPI_DFF;						// Possible values from @SPI_DFF
+	uint8_t SPI_CPOL;						// Possible values from @SPI_CPOL
+	uint8_t SPI_CPHA;						// Possible values from @SPI_CPHA
+	uint8_t SPI_SSM;						// Possible values from @SPI_SSM
+} SPI_Config_t;
+
+// Handle structure for a GPIO pin
+
+typedef struct
+{
+	SPI_TypeDef* p_SPIx;						// Holds the base address of the SPI peripheral 
+	SPI_Config_t SPI_Config;					// Holds SPI peripheral configuration settings
+} SPI_Handle_t;
+
+// Function return status
+
+typedef enum {
+	SPI_OK = 0,							// Success
+	SPI_ERROR_INVALID_STATE,					// Invalid state of a argument
+	SPI_ERROR_NULL_PTR,						// NULL pointer passed
+	SPI_ERROR_INVALID_PORT,						// Invalid SPI port address
+	SPI_ERROR_INVALID_IRQ,						// Invalid IRQ number
+	SPI_ERROR_INVALID_MODE,						// Mode value out of range
+	SPI_ERROR_INVALID_BUS_CONFIG,					// Bus config value out of range
+	SPI_ERROR_INVALID_SCLK_SPEED,					// SCLK speed value out of range
+	SPI_ERROR_INVALID_DFF,						// DFF value out of range
+	SPI_ERROR_INVALID_CPOL,						// CPOL value out of range
+	SPI_ERROR_INVALID_CPHA,						// CPHA value out of range
+	SPI_ERROR_INVALID_SSM,						// SSM value out of range
+	SPI_ERROR_TIMEOUT,						// SPI polling timeout
+	SPI_BUSY							// SPI Tx / Rx busy
+} SPI_status_t;
+
+// USAGE: --- @SPI_DEVICE_MODE ---
+
+#define SPI_DEVICE_MODE_MASTER			1
+#define SPI_DEVICE_MODE_SLAVE			0
+
+// USAGE: --- @SPI_BUS_CONFIG ---
+
+#define SPI_BUS_CONFIG_FD			3
+#define SPI_BUS_CONFIG_HD			2
+#define SPI_BUS_CONFIG_SIMPLEX_TX_ONLY		1
+#define SPI_BUS_CONFIG_SIMPLEX_RX_ONLY		0
+
+// USAGE: --- @SPI_SCLK_SPEED ---
+
+#define SPI_SCLK_SPEED_DIV_2			7
+#define SPI_SCLK_SPEED_DIV_4			6
+#define SPI_SCLK_SPEED_DIV_8			5
+#define SPI_SCLK_SPEED_DIV_16			4
+#define SPI_SCLK_SPEED_DIV_32			3
+#define SPI_SCLK_SPEED_DIV_64			2
+#define SPI_SCLK_SPEED_DIV_128			1
+#define SPI_SCLK_SPEED_DIV_256			0
+
+// USAGE: --- @SPI_DFF ---
+
+#define SPI_DFF_8BIT				1
+#define SPI_DFF_16BIT				0
+
+// USAGE: --- @SPI_CPOL ---
+
+#define SPI_CPOL_HIGH				1
+#define SPI_CPOL_LOW				0
+
+// USAGE: --- @SPI_CPHA ---
+
+#define SPI_CPHA_HIGH				1
+#define SPI_CPHA_LOW				0
+
+// USAGE: --- @SPI_SSM ---
+
+#define SPI_SSM_HW				1
+#define SPI_SSM_SW				0
+
+// NOTE: --- SPI Validation macros ---
+
+#define VALIDATE_SPI_PORT(port)		do { \
+							if ((port) == NULL || \
+							!((port) == SPI1 || (port) == SPI2 || (port) == SPI3 || (port) == SPI4)) { \
+								return SPI_ERROR_INVALID_PORT; \
+							} \
+						} while(0)
+
+#define VALIDATE_SPI_DEVICE_MODE(mode)		VALIDATE_ENUM((mode), SPI_DEVICE_MODE_MASTER, SPI_ERROR_INVALID_MODE)
+#define VALIDATE_SPI_BUS_CONFIG(stop)		VALIDATE_ENUM((stop), SPI_BUS_CONFIG_FD, SPI_ERROR_INVALID_BUS_CONFIG)
+#define VALIDATE_SPI_SCLK_SPEED(wlen)		VALIDATE_ENUM((wlen), SPI_SCLK_SPEED_DIV_2, SPI_ERROR_INVALID_SCLK_SPEED)
+#define VALIDATE_SPI_DFF(parity)		VALIDATE_ENUM((parity), SPI_DFF_8BIT, SPI_ERROR_INVALID_DFF)
+#define VALIDATE_SPI_CPOL(flow)			VALIDATE_ENUM((flow), SPI_CPOL_HIGH, SPI_ERROR_INVALID_CPOL)
+#define VALIDATE_SPI_CPHA(flow)			VALIDATE_ENUM((flow), SPI_CPHA_HIGH, SPI_ERROR_INVALID_CPHA)
+#define VALIDATE_SPI_SSM(flow)			VALIDATE_ENUM((flow), SPI_SSM_HW, SPI_ERROR_INVALID_SSM)
+
+// NOTE: --- Bit position definitions SPI_SR ---
+
+#define SPI_SR_FRE				8
+#define SPI_SR_BSY				7
+#define SPI_SR_OVR				6
+#define SPI_SR_MODF				5
+#define SPI_SR_CRCERR				4
+#define SPI_SR_UDR				3
+#define SPI_SR_CHSIDE				2
+#define SPI_SR_TXE				1
+#define SPI_SR_RXNE				0
+
+// NOTE: --- Bit position definitions SPI_CR1 ---
+
+#define SPI_CR1_BIDIMODE			15
+#define SPI_CR1_BIDIOE				14
+#define SPI_CR1_CRCEN				13
+#define SPI_CR1_CRCNEXT				12
+#define SPI_CR1_DFF				11
+#define SPI_CR1_RXONLY				10
+#define SPI_CR1_SSM				9
+#define SPI_CR1_SSI				8
+#define SPI_CR1_LSB_FIRST			7
+#define SPI_CR1_SPE				6
+#define SPI_CR1_BR				3
+#define SPI_CR1_MSTR				2
+#define SPI_CR1_CPOL				1
+#define SPI_CR1_CPHA				0
+
+// NOTE: --- Bit position definitions SPI_CR2 ---
+
+#define SPI_CR2_TXEIE				7
+#define SPI_CR2_RXNEIE				6
+#define SPI_CR2_ERRIE				5
+#define SPI_CR2_FRF				4
+#define SPI_CR2_SSOE				2
+#define SPI_CR2_TXDMAEN				1
+#define SPI_CR2_RXDMAEN				0
+
+// Peripheral clock setup
+SPI_status_t SPI_peri_clk_control(SPI_TypeDef* p_SPI_x, uint8_t EN_or_DI);
+
+// Init / de-init
+SPI_status_t SPI_init(SPI_Handle_t *p_SPI_handle);
+SPI_status_t SPI_de_init(SPI_TypeDef* p_SPI_x);
+
+// Data send / receive ( polling / interrupt ) 
+SPI_status_t SPI_write_data_pl(SPI_Handle_t* p_SPI_handle, const uint8_t* p_tx_buffer, uint32_t len);
+SPI_status_t SPI_read_data_pl(SPI_Handle_t* p_SPI_handle, uint8_t* p_rx_buffer, uint32_t len);
+SPI_status_t SPI_write_data_it(SPI_Handle_t *p_SPI_handle,uint8_t *p_tx_buffer, uint32_t len);
+SPI_status_t SPI_read_data_it(SPI_Handle_t *p_SPI_handle, uint8_t *p_rx_buffer, uint32_t len);
+
+// Peripheral control API
+
+// IRQ configuration and ISR handling
+SPI_status_t SPI_irq_interrupt_config(uint8_t irq_n, uint8_t EN_or_DI);
+SPI_status_t SPI_irq_priority_config(uint8_t irq_n, uint32_t irq_prio);
+void SPI_irq_handling(SPI_Handle_t *p_SPI_handle);
+
+#endif
